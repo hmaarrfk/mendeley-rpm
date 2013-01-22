d00b52af84e42763b55543685b09c1159dcd0db0
Mark Harfouche <mark.harfouche@gmail.com>
2013-01-21 18:45:17 -0800

    I think it is more or less finalized. I'm going to post this to the Fedora forums

--- package-mendeley-rpm.sh
+++ package-mendeley-rpm.sh
@@ -9,29 +9,27 @@ BASEDIR=$(dirname $0)
 rpmdev-setuptree
 
 mendeley_binary_archive=mendeleydesktop-1.7.1-linux-x86_64.tar.bz2
-mendeley_binary=mendeleydesktop-1.7.1-linux-x86_64
 
-tar -xf ${mendeley_binary_archive}
-
+rpm -rf ~/rpmbuild
 
 
 version=`grep Version mendeley.spec | awk '{print $2}'`
 mkdir -p ./BUILD/mendeley-${version}
 
-cp -R ${mendeley_binary}/* ./BUILD/mendeley-${version}/.
-cp mendeley-0.1.0-install.patch ./BUILD/mendeley-${version}/.
+#cp -R ${mendeley_binary_archive} ./BUILD/mendeley-${version}/.
+cp README ./BUILD/mendeley-${version}/.
+cp mendeleydesktop ./BUILD/mendeley-${version}/.
 cp mendeley.spec ./BUILD/mendeley-${version}/.
-#cp LICENSE ./BUILD/mendeley-${version}/.
+
 
 cp mendeley.spec ~/rpmbuild/SPECS/.
 
 cp mendeley-0.1.0-install.patch ~/rpmbuild/SOURCES/.
 
 tar czf ~/rpmbuild/SOURCES/mendeley-${version}.tar.gz -C ./BUILD mendeley-${version}
+cp ${mendeley_binary_archive} ~/rpmbuild/SOURCES/
 
-
-#mendeley_binary=`grep Source1 mendeley.spec | awk '{print $2}'`
-
-#cp ${mendeley_binary} ~/rpmbuild/SOURCES/.
+rm -rf ./BUILD
 
 rpmbuild -ba ~/rpmbuild/SPECS/mendeley.spec
+
